fc2d6fdc4d7cf37df45157020511e4af5522bc79
Cole Cochran <colecochran405@gmail.com>
2021-11-16 13:12:29 -0600

    creating activities get route

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,21 @@
 const router = require("express").Router();
 const activities = require ("../models/activities");
 
-router.get("/exercise", (req, res) => {
-    res.sendFile('./exercise.html', {root: "public" })
+router.get("/activities", (req, res) => {
+    activities.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" }
+        }
+      }
+    ])
+      .then(dbWorkout => {
+        console.log(dbWorkout)
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        console.log('workouts: ' + err)
+        res.status(400).json(err);
+      });
 });
+
